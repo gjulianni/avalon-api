fce0fb851ffc0e528a3ff96382223dd27d42b213
gjulianni <gabrieljulianni24@gmail.com>
2026-04-09 17:41:10 -0300

    c

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-import path from 'path';
-
-export const dbPath = path.join(process.cwd(), 'dev.db');
-process.env.DATABASE_URL = `file:${dbPath}`;
 
 export const prisma = new PrismaClient();
