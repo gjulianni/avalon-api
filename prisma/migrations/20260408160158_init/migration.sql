3cc07be9b6fc9539b41fdf618fb8043a7a645eb4
gjulianni <gabrieljulianni24@gmail.com>
2026-04-11 16:59:22 -0300

    migrations reset

--- prisma/migrations/20260408160158_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "PendingCommand" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "command" TEXT NOT NULL,
-    "executed" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
